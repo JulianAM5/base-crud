217942f97a76a7f9955d2c56cf2804b0e008c82f
JulianAM5 <julian760x@outlook.com>
2026-08-18 00:32:20 -0700

    Ventana para añadir personas

---
+++ src/main/java/bubu/julian/GUI/VentanaAñadir.java
@@ -0,0 +1,48 @@
+package bubu.julian.GUI;
+
+import bubu.julian.Controlador;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+/**
+ * VentanaAñadir
+ */
+public class VentanaAñadir extends BorderPane {
+
+    Controlador controlador;
+
+    public VentanaAñadir(Controlador controlador) {
+        this.controlador = controlador;
+        setMaxWidth(400);
+        mostrarAñadirNuevaPersona();
+    }
+
+    public void mostrarAñadirNuevaPersona() {
+        VBox holder = new VBox();
+
+        Label nombreLabel = new Label("Nombre");
+        TextField nombreField = new TextField();
+
+        Label direccionLabel = new Label("Direccion");
+        TextField direccionField = new TextField();
+
+        HBox botones = new HBox();
+        Button confirmarButton = new Button("Confirmar");
+        Button cancelarButton = new Button("Cancelar");
+        botones.getChildren().addAll(confirmarButton, cancelarButton);
+        botones.setSpacing(40);;
+        botones.setAlignment(Pos.CENTER);
+
+        holder.setSpacing(20);
+        holder.getChildren().addAll(nombreLabel, nombreField, direccionLabel, direccionField, botones);
+        holder.setAlignment(Pos.CENTER);
+        setAlignment(holder, Pos.CENTER);
+
+        setCenter(holder);
+    }
+}
